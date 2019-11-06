c99b9d8f4d7a61fdc64305dbea4230cc27023b8d
tanhaifang1213 <2201956993@qq.com>
2019-11-07 00:10:17 +0800

    本地仓库接收

---
+++ gitpro/src/com/thf/git/Demo1.java
@@ -0,0 +1,13 @@
+package com.thf.git;
+
+/**
+ * @author tanhaifang
+ * @site www.thf.com
+ * @company
+ * @create 2019-11-06 23:41
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea git 讲解");
+    }
+}
